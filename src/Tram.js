4793395fad441290bb202480acb9a2fe158ec1c7
Marc Vollenweider <marc@vollenweider.co>
2024-01-19 11:27:59 +0100

    Moved collisionArea to Tram class

--- src/Tram.js
+++ src/Tram.js
@@ -5,11 +5,11 @@ export default class Tram {
 
   buildTram(k) {
     const tramSections = [
-      { spriteId: 1, width: 138, height: 138, frame: 0, pos: { x: 0, y: 0 }, door: { x: 37, y: 62, width: 57, height: 56 } },
-      { spriteId: 2, width: 188, height: 138, frame: 0, pos: { x: 138, y: 0 }, door: { x: 11, y: 62, width: 57, height: 56 } },
-      { spriteId: 3, width: 188, height: 138, frame: 0, pos: { x: 326, y: 0 }, door: { x: 11, y: 62, width: 57, height: 56 } },
-      { spriteId: 4, width: 102, height: 138, frame: 0, pos: { x: 514, y: 0}, door: { x: 11, y: 62, width: 57, height: 56 } },
-      { spriteId: 5, width: 125, height: 138, frame: 0, pos: { x: 616, y: 0}, door: { x: 11, y: 62, width: 57, height: 56 } },
+      { spriteId: 1, width: 138, height: 138, frame: 0, pos: { x: 0, y: 0 }, door: { x: 37, y: 62, width: 57, height: 56 }, collisionArea: { left: { x: 50, y: 125, tags: ["doorLeftLast"] }, right: { x: 103, y: 125, tags: [] }} },
+      { spriteId: 2, width: 188, height: 138, frame: 0, pos: { x: 138, y: 0 }, door: { x: 11, y: 62, width: 57, height: 56 }, collisionArea: { left: { x: 150, y: 125, tags: [] }, right: { x: 203, y: 125, tags: [] }} },
+      { spriteId: 3, width: 188, height: 138, frame: 0, pos: { x: 326, y: 0 }, door: { x: 11, y: 62, width: 57, height: 56 }, collisionArea: { left: { x: 339, y: 125, tags: [] }, right: { x: 391, y: 125, tags: [] }} },
+      { spriteId: 4, width: 102, height: 138, frame: 0, pos: { x: 514, y: 0}, door: { x: 11, y: 62, width: 57, height: 56 }, collisionArea: { left: { x: 526, y: 125, tags: [] }, right: { x: 579, y: 125, tags: [] }} },
+      { spriteId: 5, width: 125, height: 138, frame: 0, pos: { x: 616, y: 0}, door: { x: 11, y: 62, width: 57, height: 56 }, collisionArea: { left: { x: 629, y: 125, tags: [] }, right: { x: 681, y: 125, tags: ["doorRightLast"] }} },
     ];
 
     tramSections.forEach((section) => {
@@ -39,6 +39,27 @@ export default class Tram {
           tramSection.frame = 1;
         }
       });
+
+      //Add collisionAreas
+      k.add([
+        rect(5, 50),
+        pos(this.pos.x + section.collisionArea.left.x, this.pos.y + section.collisionArea.left.y),
+        color(0, 0, 0),
+        opacity(0),
+        area(),
+        "doorLeft",
+        ...section.collisionArea.left.tags
+      ]);
+
+      k.add([
+        rect(5, 50),
+        pos(this.pos.x + section.collisionArea.right.x, this.pos.y + section.collisionArea.right.y),
+        color(0, 0, 0),
+        opacity(0),
+        area(),
+        "doorRight",
+        ...section.collisionArea.right.tags
+      ]);
     });
   }
 }
